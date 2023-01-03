b4fb2eea41a7d158aa92e898cec3adfd71fdead7
juniorovi <mdtarikulislam258000@gmail.com>
2023-01-03 23:02:33 +0600

    Destracturing added for data pass easily

--- src/components/Countries/Country/Country.js
+++ src/components/Countries/Country/Country.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    const {name, population, area}=props.country;
     return (
         <div className='country'>
-            <h1>Country Name: {props.country.name.common}</h1>
-            <p>Populations: {props.country.population}</p>
-            <p>Area: {props.country.area}</p>
+            <h1>Country Name: {name.common}</h1>
+            <p>Populations: {population}</p>
+            <p>Area: {area}</p>
         </div>
     );
 };
